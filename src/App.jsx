f638d49515c980c9ad44bfeee08a54119479e4db
Julius Caesar <tokokalandadze@gmail.com>
2025-05-15 00:25:34 +0400

    Recreated project from scratch

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './index.css';
+
+function App() {
+  return (
+    <div>
+      <h1 className="text-3xl underline">Hello, clean React + Vite!</h1>
+    </div>
+  );
+}
+
+export default App;
